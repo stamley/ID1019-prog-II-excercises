d9c1c4cf93ba422269da9f4f96194ef297d0dc4d
stamley <axellystam@gmail.com>
2023-01-31 15:55:01 +0100

    Div cases and quotients aswell as undefined

--- exercises/A3_Evaluate_expression/Eval.ex
+++ exercises/A3_Evaluate_expression/Eval.ex
@@ -9,40 +9,59 @@ defmodule Eval do
   | {:quot, number(), number()}
 
   # Returns environment with given bindings of variables
-  def env([]) do nil end
+  # Also finds given binding of a variable
+  def env([{var, val}]) do [{{:var, var}, {:num, val}}] end
   def env([{var, val} | tail]) do
     [{{:var, var}, {:num, val}} | env(tail)]
   end
-  def env({:var, var}, []) do var end
-  # If there exists a binding in the environment return the value
-  def env({:var, var}, [{{:var, var}, {:num, val}}|_tail]) do val end
-  # Continue looking...
-  def env(var, [head|tail]) do env(var, tail) end
+  def env({:var, var}, []) do {:var, var} end
+  def env({:var, var}, [{{:var, var}, {:num, val}}|_tail]) do {:num, val} end
+  def env(var, [_head|tail]) do env(var, tail) end
 
-
-  def eval({:num, n}, _) do n end
-  # If the variable has a binding, find it
+  # Finds the binding of a variable or reduces quotients
+  def eval(:undefined, _) do :error end
+  def eval({:num, n}, _env) do {:num, n} end
   def eval({:var, var}, env) do env({:var, var}, env) end
-  # Evaluate two expressions and then add them together
-  def eval({:add, e1, e2}, env) do add(eval(e1), eval(e2)) end
-  def eval({:sub, e1, e2}, env) do sub(eval(e1), eval(e2)) end
-  def eval({:mul, e1, e2}, env) do mul(eval(e1), eval(e2)) end
-  def eval({:div, e1, e2}, env) do div(eval(e1), eval(e2)) end
+  def eval({:quot, n1, n2}, _) do reduce({:quot, n1, n2}) end
 
-  def add({:num, n1}, {:num, n2}) do n1 + n2 end
-  def sub({:num, n1}, {:num, n2}) do n1 - n2 end
-  def mul({:num, n1}, {:num, n2}) do n1 * n2 end
-  # Simplify this multiplication, if n1*q1/q2 can be reduced,
-  # it should be.
-  # Possible without checking division with each natural number?
-  def mul({:num, n1}, {:q, q1, q2}) do {:q, n1*q1, q2} end
-  def mul({:num, n1}, {:q, q1, q2}) do {:q, n1*q1/2, q2/2} when rem(n1*q1, 2) == 0 and rem(q2, 2) == 0  end
-  def div({:num, n1}, {:num, n2}) do n1 / n2 when rem(n1, n2) == 0 end
-  def div({:num, n1}, {:num, n2}) do {:quot, n1, n2} end
+  # Evaluate two expressions and perform the operation
+  def eval({:add, e1, e2}, env) do eval(add(eval(e1, env), eval(e2, env)), env) end
+  def eval({:sub, e1, e2}, env) do eval(sub(eval(e1, env), eval(e2, env)), env) end
+  def eval({:mul, e1, e2}, env) do eval(mul(eval(e1, env), eval(e2, env)), env) end
+  def eval({:div, e1, e2}, env) do eval(divi(eval(e1, env), eval(e2, env)), env) end
 
-  # Add, Sub, Mul, Div
-  # Numbers might be rational...
-  # Reduce rational numbers as much as possible 2*3/4 should not be
-  # 6/4 but 3/2
+  # Divide the quotient if possible, otherwise reduce it with gcd
+  def reduce({:quot, _, 0}) do :error end
+  def reduce({:quot, 0, _}) do {:num, 0} end
+  def reduce({:quot, q1, q2}) when rem(q1, q2) == 0 do {:num, div(q1, q2)} end
+  def reduce({:quot, q1, q2}) do
+    gcd = Integer.gcd(q1, q2)
+    {:quot, div(q1, gcd), div(q2, gcd)}
+  end
 
+  def add({:num, n1}, {:num, n2}) do {:num, n1 + n2} end
+  def add({:quot, q1, q2}, {:num, n}) do {:quot, q1 + n * q2, q2} end
+  def add({:num, n}, {:quot, q1, q2}) do {:quot, q1 + n * q2, q2} end
+  def add({:quot, q1, q2}, {:quot, q3, q2}) do {:quot, q1 + q3, q2} end
+  def add({:quot, q1, q2}, {:quot, q3, q4}) do {:quot, q1 * q4 + q3 * q2, q2 * q4} end
+
+  def sub({:num, n1}, {:num, n2}) do {:num, n1 - n2} end
+  def sub({:quot, q1, q2}, {:num, n}) do {:quot, q1 - n * q2, q2} end
+  def sub({:num, n}, {:quot, q1, q2}) do {:quot, n * q2 - q1, q2} end
+  def sub({:quot, q1, q2}, {:quot, q3, q2}) do {:quot, q1 - q3, q2} end
+  def sub({:quot, q1, q2}, {:quot, q3, q4}) do {:quot, q1 * q4 - q3 * q2, q2 * q4} end
+
+  def mul({:num, 0}, _) do {:num, 0} end
+  def mul(_, {:num, 0}) do {:num, 0} end
+  def mul({:num, n1}, {:num, n2}) do {:num, n1 * n2} end
+  def mul({:num, n}, {:quot, q1, q2}) do {:quot, n * q1, q2} end
+  def mul({:quot, q1, q2}, {:num, n}) do {:quot, n * q1, q2} end
+  def mul({:quot, q1, q2}, {:quot, q3, q4}) do {:quot, q1 * q3, q2 * q4} end
+
+  def divi(_, {:num, 0}) do :undefined end
+  def divi({:num, 0}, _) do {:num, 0} end
+  def divi({:num, n1}, {:num, n2}) do {:quot, n1, n2} end
+  def divi({:num, n}, {:quot, q1, q2}) do {:quot, n * q2, q1} end
+  def divi({:quot, q1, q2}, {:num, n}) do {:quot, q1, q2 * n} end
+  def divi({:quot, q1, q2}, {:quot, q3, q4}) do {:quot, q1 * q4, q2 * q3} end
 end
